cd4288b1df4445725055f01f6ca0987b257ccb69
tubaaNur <yesiltastubanur@gmail.com>
2023-08-03 23:48:50 +0300

    coffee class added

---
+++ CafeApp/data/entity/Coffee.swift
@@ -0,0 +1,29 @@
+//
+//  Coffee.swift
+//  CafeApp
+//
+//  Created by Tuba Nur  on 3.08.2023.
+//
+
+import UIKit
+
+class Coffee: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
